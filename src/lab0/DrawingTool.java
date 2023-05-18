bf8df63f371e8736b46f4bb7c2cfac8b48dc608d
Ole Pearse-Danker <ole.pd@icloud.com>
2023-05-18 17:30:31 +0200

    Added JPanel with Button and Splitpane

--- src/lab0/DrawingTool.java
+++ src/lab0/DrawingTool.java
@@ -2,8 +2,8 @@
 
 package lab0;
 
-import javax.swing.JFrame;
-import java.awt.Dimension;
+import javax.swing.*;
+import java.awt.*;
 
 @SuppressWarnings("serial")
 public class DrawingTool extends JFrame {
@@ -12,7 +12,29 @@ public class DrawingTool extends JFrame {
     public DrawingTool(String title) {
         super(title);
 
-        constructDrawingArea();
+        // New SplitPane
+        JSplitPane splitPane = new JSplitPane();
+        splitPane.setOrientation(JSplitPane.VERTICAL_SPLIT);
+        splitPane.setDividerLocation(500);
+
+        //constructDrawingArea();
+        drawing = new DrawingArea();
+
+        // Add new panel to the frame
+        JPanel gUIPanel = new JPanel();
+        gUIPanel.setBounds(0, 500, 300, 60);
+        gUIPanel.setBackground(Color.BLACK);
+
+        // Add new button to the panel
+        JButton button = new JButton("Add Cat");
+        gUIPanel.add(button);
+
+        // Add panels to the split pane
+        splitPane.setTopComponent(drawing);
+        splitPane.setBottomComponent(gUIPanel);
+
+        // Add SplitPane to the frame
+        this.add(splitPane);
 
         Dimension screenSize = getToolkit().getScreenSize();
         setBounds(0, 0, screenSize.width, screenSize.height);
